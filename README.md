bb9ad9f07385a366c9c56503f62d60c31e20a48e
Manuel Wedler <manuel@wedler.dev>
2024-10-10 09:44:17 +0200

    Add multi-proxy-analysis script

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ npm install
 
 It is necessary to have a `.env` file with the environment variables that can be found in the example `.env.template` file.
 
+Additionally, `multi-proxy-analysis` requires RPC endpoints to resolve implementation addresses. Please create a `rpc-config.json` in the format that the `rpc-config.json.template` shows. The keys are the chain IDs and the values are the RPC URLs. Add an RPC URL for all the chains that you want to analyse.
+
 
 ## Available scripts
 
@@ -26,6 +28,11 @@ npm run proxy-analysis
 
 This script detects proxy contracts in a Sourcify database. It uses the `whatsabi` library to detect proxy contracts by their bytecode. The results from a run on the whole production database of Sourcify on the 6th October 2024 can be found in the `proxy-analysis-results` directory.
 
+### Multi-proxy analysis
 
+```bash
+npm run multi-proxy-analysis
+```
 
+This script runs on the results of `proxy-analysis`. It makes use of the `proxy-detection-results/multi-proxy-contracts.json` file. It gives the different detected proxy types and how many different implementation addresses they resolve to. The results from a run on the 11th October 2024 can be found in the `multi-proxy-analysis-results` directory.
 
---
+++ multi-proxy-analysis.mjs
@@ -0,0 +1,228 @@
+// Takes the result of proxy-analysis.mjs and analyses the multi-proxy contracts
+import fs from "fs/promises";
+import path from "path";
+import pg from "pg";
+import dotenv from "dotenv";
+import process from "process";
+import { fileURLToPath } from "url";
+import { whatsabi } from "@shazow/whatsabi";
+import { JsonRpcProvider } from "@ethersproject/providers";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+const PROXY_RESULT_FOLDER = "proxy-detection-results";
+const MULTI_PROXY_RESULT_FOLDER = "multi-proxy-analysis-results";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+async function writeJsonFile(filename, data) {
+  const outputPath = path.join(__dirname, MULTI_PROXY_RESULT_FOLDER, filename);
+  try {
+    await fs.mkdir(path.dirname(outputPath), { recursive: true });
+    await fs.writeFile(outputPath, JSON.stringify(data, null, 2));
+    console.log(`Data written to ${outputPath}`);
+  } catch (writeError) {
+    console.error("Error writing to file:", writeError);
+  }
+}
+
+const getStringFromResolverList = (proxyResolverList) => {
+  return proxyResolverList.map((resolver) => resolver.name).join(", ");
+};
+
+async function checkMultiProxies() {
+  const multiProxyContractsPath = path.join(
+    __dirname,
+    PROXY_RESULT_FOLDER,
+    "multi-proxy-contracts.json"
+  );
+  const multiProxyContracts = JSON.parse(
+    await fs.readFile(multiProxyContractsPath, "utf8")
+  );
+
+  const rpcConfigPath = path.join(__dirname, "rpc-config.json");
+  const rpcConfig = JSON.parse(await fs.readFile(rpcConfigPath, "utf8"));
+
+  const client = await pool.connect();
+
+  let processedContracts = 0;
+  const checkedChains = [];
+  const multiProxyContractTypes = {};
+  const numberOfImplementationAddressesCount = {};
+  const multiImplementationAddresses = {};
+  const diamondProxies = {};
+
+  const printResults = async () => {
+    console.log();
+    console.log("Processed contracts: ", processedContracts);
+    console.log("Checked chains: ", checkedChains);
+    console.log("Multi proxy contract types: ", multiProxyContractTypes);
+    console.log(
+      "Number of implementation addresses count: ",
+      numberOfImplementationAddressesCount
+    );
+
+    const result = {
+      processedContracts,
+      checkedChains,
+      numberOfImplementationAddressesCount,
+      multiProxyContractTypes,
+    };
+    await writeJsonFile("result.json", result);
+    await writeJsonFile(
+      "multi-implementation-addresses.json",
+      multiImplementationAddresses
+    );
+    await writeJsonFile("diamond-proxies.json", diamondProxies);
+  };
+  process.on("SIGINT", async () => {
+    await printResults();
+    process.exit(0);
+  });
+
+  try {
+    for (const chainId of Object.keys(multiProxyContracts)) {
+      if (!rpcConfig[chainId]) {
+        console.warn(`No RPC URL for chain id ${chainId}`);
+        continue;
+      }
+      const provider = new JsonRpcProvider(rpcConfig[chainId]);
+      console.log("Processing chain id ", chainId);
+      checkedChains.push(chainId);
+
+      for (const address of multiProxyContracts[chainId]) {
+        const queryResult = await client.query(
+          `
+            SELECT
+              encode(code.code, 'hex') as code
+            FROM sourcify_matches
+              JOIN verified_contracts ON verified_contracts.id = sourcify_matches.verified_contract_id
+              JOIN contract_deployments ON contract_deployments.id = verified_contracts.deployment_id
+              JOIN contracts ON contracts.id = contract_deployments.contract_id
+              JOIN code on code.code_hash = contracts.runtime_code_hash
+            WHERE decode($1, 'hex') = contract_deployments.address
+              AND contract_deployments.chain_id = $2
+          `,
+          [address.substring(2), chainId]
+        );
+
+        if (queryResult.rows.length > 1) {
+          console.error(
+            `Multiple contracts found for address ${address} and chain id ${chainId}`
+          );
+          console.warn(`Skipping address ${address} on chain ${chainId}`);
+          continue;
+        }
+
+        processedContracts++;
+
+        const row = queryResult.rows[0];
+
+        // Proxy detection
+        const codeCache = {
+          [address]: row.code,
+        };
+        const cachedCodeProvider = whatsabi.providers.WithCachedCode(
+          provider,
+          codeCache
+        );
+        let whatsabiResult;
+        try {
+          whatsabiResult = await whatsabi.autoload(address, {
+            provider: cachedCodeProvider,
+            abiLoader: false,
+            signatureLookup: false,
+            followProxies: false,
+          });
+        } catch (error) {
+          console.error("Error in whatsabi.autoload: ", error);
+          console.warn(`Skipping address ${address} on chain ${chainId}`);
+          continue;
+        }
+
+        if (whatsabiResult.proxies.length < 2) {
+          console.warn(
+            `Just ${whatsabiResult.proxies.length} proxies found for address ${address} on chain ${chainId}`
+          );
+          continue;
+        }
+
+        const resolverListString = getStringFromResolverList(
+          whatsabiResult.proxies
+        );
+        if (!multiProxyContractTypes[resolverListString]) {
+          multiProxyContractTypes[resolverListString] = 0;
+        }
+        multiProxyContractTypes[resolverListString]++;
+
+        if (
+          whatsabiResult.proxies.some((proxy) => proxy.name === "DiamondProxy")
+        ) {
+          if (!diamondProxies[chainId]) {
+            diamondProxies[chainId] = [];
+          }
+          diamondProxies[chainId].push(address);
+          continue;
+        }
+
+        const implementations = new Set();
+        for (const proxy of whatsabiResult.proxies) {
+          let implementation;
+          try {
+            implementation = await proxy.resolve(provider, address);
+          } catch (error) {
+            console.error("Error in proxy.resolve: ", error);
+            console.warn(
+              `Skipping this proxy ${proxy.name} for address ${address} on chain ${chainId}`
+            );
+            continue;
+          }
+          implementations.add(implementation);
+        }
+
+        if (!numberOfImplementationAddressesCount[implementations.size]) {
+          numberOfImplementationAddressesCount[implementations.size] = 0;
+        }
+        numberOfImplementationAddressesCount[implementations.size]++;
+
+        if (implementations.size > 1) {
+          if (!multiImplementationAddresses[chainId]) {
+            multiImplementationAddresses[chainId] = {};
+          }
+          multiImplementationAddresses[chainId][address] = [
+            getStringFromResolverList(whatsabiResult.proxies),
+            ...Array.from(implementations),
+          ];
+        }
+      }
+    }
+
+    // Done
+  } catch (error) {
+    console.error("Error checking multi proxies: ", error);
+  } finally {
+    client.release();
+    await printResults();
+  }
+}
+
+checkMultiProxies()
+  .then(() => {
+    console.log("Completed");
+  })
+  .catch((error) => {
+    console.error("Error in checkMultiProxies: ", error);
+  })
+  .finally(() => {
+    pool.end();
+  });
